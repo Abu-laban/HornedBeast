7d7803b03921044a4f025c000d14c5b678d7d198
Tariq Abulaban <tariqabulaban@hotmail.com>
2021-06-27 20:00:42 +0300

    finish lab01

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <p>the author: Tariq Abu-laban</p>
+        )
+    }
+}
+
+export default Footer;
